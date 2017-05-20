bdd9be986b7811b079fba5beddaa56cfcbaa1bec
Jonathan Lowell <elderlowell@gmail.com>
2017-05-20 11:34:58 -0600

    finished fourth problem

--- arrayReview.js
+++ arrayReview.js
@@ -57,6 +57,8 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
 var second;
+second = first.slice(0,first.length);
+second.push(6,7);
 
   //code here
 
