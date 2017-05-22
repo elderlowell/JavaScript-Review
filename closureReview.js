738039783d7fdbf44e26eb7ae6db5f4e9d81564c
Jonathan Lowell <elderlowell@gmail.com>
2017-05-22 17:03:49 -0600

    still not working

--- closureReview.js
+++ closureReview.js
@@ -6,10 +6,12 @@ var callFriend = function(){
   return callF;
 };
 
-//Above you're given a callFriend function that returns another function. 
+//Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //code here
+  var callJake = callFriend();
+  callJake('435-215-9248');
 
 
 
@@ -20,3 +22,12 @@ Write a function that accepts a function as it's first argument and returns a ne
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 
 */
+
+function stuff(cb, n) {
+  for (var i = 0; i < n; i++) {
+    return function () {
+      cb();
+    }
+  }
+  console.log('STAHHP');
+}
